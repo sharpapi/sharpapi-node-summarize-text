a09a1f7d09cf6c88a251a715be6c9728206f1825
Dawid Makowski <dawid.makowski@gmail.com>
2026-01-09 23:34:32 +0700

    Update to use NPM core package and add custom User-Agent
    - Changed @sharpapi/sharpapi-node-core dependency from local file to ^1.0.0
    - Added custom User-Agent: @sharpapi/sharpapi-node-summarize-text/1.0.1
    - Bumped version to 1.0.1

--- src/SharpApiSummarizeTextService.js
+++ src/SharpApiSummarizeTextService.js
@@ -5,6 +5,15 @@ const { SharpApiCoreService, SharpApiJobTypeEnum } = require('@sharpapi/sharpapi
  */
 class SharpApiSummarizeTextService extends SharpApiCoreService {
   /**
+   * Creates a new SharpApiSummarizeTextService instance
+   * @param {string} apiKey - Your SharpAPI API key
+   * @param {string} [apiBaseUrl='https://sharpapi.com/api/v1'] - API base URL
+   */
+  constructor(apiKey, apiBaseUrl = 'https://sharpapi.com/api/v1') {
+    super(apiKey, apiBaseUrl, '@sharpapi/sharpapi-node-summarize-text/1.0.1');
+  }
+
+  /**
    * Generates a summarized version of the provided content.
    * Perfect for generating marketing introductions of longer texts.
    *
